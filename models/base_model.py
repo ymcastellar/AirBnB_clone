ee76274dd5a5f9794ac61a046958215b05d5cea7
ymcastellar <yoycas@hotmail.com>
2020-06-27 17:07:47 -0500

    added kargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,30 @@ from datetime import datetime
 class BaseModel:
     """Base Model class"""
 
-    def __init__(self):
-        """Initialization
+    def __init__(self, *args, **kwargs):
+        """Initialization Base instance
         """
-        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if hasattr(self, "created_at")
+                and type(self.created_at) == str:
+                    self.created_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                if hasattr(self, "updated_at")
+                and type(self.updated_at) == str:
+                    self.updated_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         """__str__ module
         Returns:
-            [str]: human readable representation of an instance
+          [str]: human readable representation of an instance
         """
         return "[{}] ({}) {}"\
             .format(type(self).__name__, self.id, self.__dict__)

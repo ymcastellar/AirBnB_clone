0ec40d3d7afc591c605e7c051993137690497aea
ymcastellar <yoycas@hotmail.com>
2020-06-30 12:41:11 -0500

    added test content to file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""[summary]
+"""
+import unittest
+
+class TestBaseModel(unittest.TestCase):
+    """Test class for base models."""
+
+
+
+if __name__ == '__main__':
+    unittest.main()
